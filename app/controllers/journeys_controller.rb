aaae41a7bce0ef83792537286afec39eded2ef9c
Tobias <tholetzko@users.noreply.github.com>
2017-06-02 10:57:20 -0500

    Added Google Maps API

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,4 +1,10 @@
+require 'open-uri'
+require 'json'
+
+
 class JourneysController < ApplicationController
+
+
   def index
     @journeys = Journey.all
 
@@ -7,7 +13,11 @@ class JourneysController < ApplicationController
 
   def show
     @journey = Journey.find(params[:id])
-
+    @street_address = @journey.origin
+      url = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyB_nAWVr-18Oi_XoadzVHmNT2vevvJfev4&address="+@street_address.gsub(" ","+")
+      parsed_data = JSON.parse(open(url).read)
+      @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+      @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
     render("journeys/show.html.erb")
   end
 
@@ -26,6 +36,7 @@ class JourneysController < ApplicationController
     @journey.date = params[:date]
     @journey.price = params[:price]
     @journey.organizer_id = params[:organizer_id]
+
 
     save_status = @journey.save
 
@@ -69,7 +80,7 @@ class JourneysController < ApplicationController
     if URI(request.referer).path == "/journeys/#{@journey.id}"
       redirect_to("/", :notice => "Journey deleted.")
     else
-      redirect_to(:back, :notice => "Journey deleted.")
+      redirect_to("/journeys", :notice => "Journey deleted.")
     end
   end
 end
