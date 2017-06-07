0053d2b583ee564547d15c7b4d413953147941d1
Tobias <tholetzko@users.noreply.github.com>
2017-06-06 21:10:06 -0500

    Updated index page

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -24,50 +24,53 @@ class SeatsController < ApplicationController
 
     if (Seat.where(:journey_id => @seat.journey_id).count + 1) > Journey.find_by(:id => @seat.journey_id).capacity
 
-      redirect_to("/journeys", :alert => "No capacity.")
+      redirect_to("/journeys", :alert => "Seat already taken.")
 
-    else
+      else
+
+        save_status = @seat.save
+
+        if save_status == true
+          redirect_to("/journeys/#{Journey.find_by(:id => @seat.journey_id).id}", :notice => "Seat created successfully.")
+
+        else
+          render("/journeys")
+        end
+
+      end
+
+    end
+
+    def edit
+      @seat = Seat.find(params[:id])
+
+      render("seats/edit.html.erb")
+    end
+
+    def update
+      @seat = Seat.find(params[:id])
+
+      @seat.journey_id = params[:journey_id]
+      @seat.user_id = params[:user_id]
 
       save_status = @seat.save
 
       if save_status == true
-        redirect_to("/journeys/#{Journey.find_by(:id => @seat.journey_id).id}", :notice => "Seat created successfully.")
+        redirect_to("/seats/#{@seat.id}", :notice => "Seat updated successfully.")
       else
-        render("seats/new.html.erb")
+        render("seats/edit.html.erb")
+      end
+    end
+
+    def destroy
+      @seat = Seat.find(params[:id])
+
+      @seat.destroy
+
+      if URI(request.referer).path == "/seats/#{@seat.id}"
+        redirect_to("/", :notice => "Seat deleted.")
+      else
+        redirect_to(:back, :notice => "Seat deleted.")
       end
     end
   end
-
-  def edit
-    @seat = Seat.find(params[:id])
-
-    render("seats/edit.html.erb")
-  end
-
-  def update
-    @seat = Seat.find(params[:id])
-
-    @seat.journey_id = params[:journey_id]
-    @seat.user_id = params[:user_id]
-
-    save_status = @seat.save
-
-    if save_status == true
-      redirect_to("/seats/#{@seat.id}", :notice => "Seat updated successfully.")
-    else
-      render("seats/edit.html.erb")
-    end
-  end
-
-  def destroy
-    @seat = Seat.find(params[:id])
-
-    @seat.destroy
-
-    if URI(request.referer).path == "/seats/#{@seat.id}"
-      redirect_to("/", :notice => "Seat deleted.")
-    else
-      redirect_to(:back, :notice => "Seat deleted.")
-    end
-  end
-end
--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,4 +1,5 @@
 class Seat < ApplicationRecord
+  validates :user_id, :uniqueness => { :scope => [:journey_id], :message => "already has a seat" }
   belongs_to :user
   belongs_to :journey
 end
